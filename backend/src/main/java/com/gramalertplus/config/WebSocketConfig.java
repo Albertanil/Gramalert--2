2b3b8ab5066b2a5149262177bd176d8eb080abcb
Albert Anil <albertanil1234@gmail.com>
2025-10-17 23:27:08 +0530

    FIX:  cors issue

--- backend/src/main/java/com/gramalertplus/config/WebSocketConfig.java
+++ backend/src/main/java/com/gramalertplus/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("http://localhost:3000","https://gramalert-2.vercel.app")
                 .withSockJS();
     }
 
